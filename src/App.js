a21fac06cd084b7ed457d409c28b5bdacf04ce4e
felipeghidini <lipeghidini18@gmail.com>
2021-09-19 08:28:28 -0300

    Initial structure

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const App = () => {
+    return (<div className="container">
+        <div className="valid-keys">
+            <span className="matched">Jes</span>
+            <span className="remainder">us</span>
+        </div>
+        <div className="typed-keys">Glória a Deus</div>
+        <div className="completed-words">
+            <ol>
+                <li>Deus</li>
+                <li>Maravilhoso</li>
+                <li>Conselheiro</li>
+                <li>Pai da eternidade</li>
+                <li>Cidade</li>
+            </ol>
+        </div>
+    </div>)
+};
+
+export default App;
